c23bb0c77e38271acecae64b8030e6a772a9b2c2
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2023-12-28 21:40:48 +0000

    updates to app.rb and added view templates to views directory

--- app.rb
+++ app.rb
@@ -1,9 +1,138 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
+# erb{:layout => false } allows you to change the layout of one or more of your erb files
+# Remember, when fetching params you're fetching the name of the dynamic route ex 
+# get(/:choices)
+# params.fetch("choices")
+
+# define a route
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+
+  # build the API url, including the API key in the query string
+  api_key = ENV.fetch("EXCHANGE_RATE_KEY")
+  
+  api_url = "http://api.exchangerate.host/list?access_key=#{api_key}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = parsed_data.fetch("currencies")
+
+  # render a view template where I show the symbols
+  erb(:homepage)
 end
+
+get("/:from_currency") do
+
+  @orignal_currency = params.fetch("from_currency")
+
+  api_key = ENV.fetch("EXCHANGE_RATE_KEY")
+  
+  api_url = "http://api.exchangerate.host/list?access_key=#{api_key}"
+
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  @symbols = parsed_data.fetch("currencies")
+
+
+  erb(:starting_currency)
+end
+
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_key = ENV.fetch("EXCHANGE_RATE_KEY")
+
+  api_url = "http://api.exchangerate.host/convert?access_key=#{api_key}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  @rate = parsed_data.fetch("result")
+  
+  erb(:destination_currency)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def currencies
+#   raw_data= HTTP.get("https://api.exchangerate.host/symbols")
+#   parsed_data = JSON.parse(raw_data)
+#   currency_keys = parsed_data.fetch("symbols").keys
+
+#   return currency_keys
+
+# end
+
+# get("/") do
+#   @symbols = currencies
+  
+#   # erb(:home)
+#   erb(:homepage)
+# end
+
+# get("/:symbol") do
+#   @symbol = params.fetch("symbol") 
+
+#   @symbols = currencies
+
+#   erb(:second_step)
+# end
+
+# get("/:symbol/:conversion") do
+#   @symbol = params.fetch("symbol")
+#   @conversion = params.fetch("conversion")
+
+#   raw_conversion = HTTP.get("https://api.exchangerate.host/convert?from=#{@symbol}&to=#{@conversion}")
+#   parsed_conversion = JSON.parse(raw_conversion)
+#   @rate = parsed_conversion.fetch("result")
+
+#   erb(:third_step)
+# end
